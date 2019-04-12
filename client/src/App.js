fabefe35a7929045aa4d7780760210e6c3dadd1c
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-12 16:40:10 -0400

    Added npm start to client

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hey, this is no longer a logo</h1>
+        <h1>Fake Reddit (The Real Reddit)</h1>
+        {/* <Router>
+          <Switch>
+            <Routes></Routes>
+          </Switch>
+        </Router> */}
       </div>
     );
   }

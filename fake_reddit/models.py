536d8b3ebbb928ac708c7fc0ab12ae2f80a1b0c5
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-12 14:44:16 -0400

    Made migrations T.M.

--- fake_reddit/models.py
+++ fake_reddit/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Comments(models.Model):
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     content = models.CharField(default='', max_length=255)
     preview_url = models.CharField(max_length=255)
     # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
@@ -12,12 +12,12 @@ class Comments(models.Model):
         return self.preview_url
 
 class Posts(models.Model):
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     picture_url = models.CharField(max_length=400)
     content = models.CharField(default='', max_length=255)
     site_url = models.CharField(max_length=400)
-    vote_total = models.IntergerField(defautlt=1)
+    vote_total = models.IntegerField(default=0)
     # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     
     def __str__(self):

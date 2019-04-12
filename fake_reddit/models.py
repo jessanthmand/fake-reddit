2f2c4f53565fa705fe151d75e5d4d6469527f43e
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-04-12 14:37:42 -0400

    created comments and posts models T.M.

--- fake_reddit/models.py
+++ fake_reddit/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Comments(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    content = models.CharField(default='', max_length=255)
+    preview_url = models.CharField(max_length=255)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.preview_url
+
+class Posts(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=255)
+    picture_url = models.CharField(max_length=400)
+    content = models.CharField(default='', max_length=255)
+    site_url = models.CharField(max_length=400)
+    vote_total = models.IntergerField(defautlt=1)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    
+    def __str__(self):
+        return self.title
